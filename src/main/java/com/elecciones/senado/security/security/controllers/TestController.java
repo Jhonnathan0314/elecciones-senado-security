8bcba598e1d855d619c831fc6ae260c35b9e6d9a
Jhonnathan0314 <jffc314@gmail.com>
2024-05-02 17:30:10 -0500

    Update Test endpoint

--- src/main/java/com/elecciones/senado/security/security/controllers/TestController.java
+++ src/main/java/com/elecciones/senado/security/security/controllers/TestController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/security/test")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class TestController {
 
-    @GetMapping(value = "test")
+    @GetMapping
     public String welcome() {
         return "Welcome to a secure endpoint";
     }
